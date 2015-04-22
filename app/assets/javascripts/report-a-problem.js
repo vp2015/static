07524832b5e80caca96ed028583cf87ef435e564
Tatiana Soukiassian <tatiana.soukiassian@digital.cabinet-office.gov.uk>
2015-04-22 16:57:12 +0100

    First try at redesigning report-a-problem
    This commit will have to be discarded or squashed as heavy
    refactoring on the report-a-problem.js is going to be merged soon
    and means we will need to take a different approach to
    redesigning the form.
    This redesign only applies to some pages, for an A/B test.

--- app/assets/javascripts/report-a-problem.js
+++ app/assets/javascripts/report-a-problem.js
@@ -1,4 +1,6 @@
-(function() {
+//= require govuk/multivariate-test
+
+(function () {
   "use strict";
 
   window.GOVUK = window.GOVUK || {};
@@ -58,27 +60,81 @@
   }
 
   $(document).ready(function() {
-    // Add in the toggle link for reporting a problem at the bottom of the page
-    var toggleBlock = '<div class="report-a-problem-toggle-wrapper js-footer">' +
-                        '<p class="report-a-problem-toggle">' +
-                          '<a href="">Is there anything wrong with this page?</a>' +
-                        '</p>' +
-                      '</div>';
-    var $container = $('.report-a-problem-container')
-    $container.before(toggleBlock);
+    if (pageIncludedInRedesign()) {
+      var test = new GOVUK.MultivariateTest({
+        name: 'report-a-problem-redesign-ab-test',
+        contentExperimentId: "SnpcHld1SJuQig-_SsaN_Q",
+        cohorts: {
+          variant_0: {variantId: 0, callback: normalReportAProblem},
+          variant_1: {variantId: 1, callback: alternate_design}
+        }
+      });
+    } else {
+      normalReportAProblem();
+    };
 
-    // Add a click handler for the toggle
-    $('.report-a-problem-toggle a').on('click', function() {
-      $container.toggle();
-      return false;
-    });
+    function pageIncludedInRedesign(){
+      return (window.location.href.indexOf("/help") > -1)
+    };
 
-    // form submission for reporting a problem
-    var $form = $container.find('form');
-    $form.append('<input type="hidden" name="javascript_enabled" value="true"/>');
-    $form.append($('<input type="hidden" name="referrer">').val(document.referrer || "unknown"));
-    $form.submit(GOVUK.reportAProblem.submit);
+    function normalReportAProblem(){
+      // Add a click handler for the toggle
+      var toggleBlock = '<div class="report-a-problem-toggle-wrapper js-footer">' +
+                          '<p class="report-a-problem-toggle">' +
+                            '<a href="">Is there anything wrong with this page?</a>' +
+                          '</p>' +
+                        '</div>';
+      var $container = $('.report-a-problem-container')
+      $container.before(toggleBlock);
 
+
+      $('.report-a-problem-toggle a').on('click', function() {
+        $container.toggle();
+        return false;
+      });
+
+      // form submission for reporting a problem
+      var $form = $container.find('form');
+      $form.append('<input type="hidden" name="javascript_enabled" value="true"/>');
+      $form.append($('<input type="hidden" name="referrer">').val(document.referrer || "unknown"));
+      $form.submit(GOVUK.reportAProblem.submit);
+
+    };
+
+    function alternate_design() {
+      // Add in the toggle link for reporting a problem at the bottom of the page
+      var toggleBlockAlternate = '<div class="report-a-problem-toggle-wrapper js-footer">' +
+                          '<p class="report-a-problem-toggle">' +
+                              '<p> Was this page useful?<p>' +
+                              '<form>' +
+                                '<fieldset class="inline"> ' +
+                                  '<label class="block-label selectable" for="radio-inline-1">' +
+                                   '<input id="radio-inline-1" type="radio" name="radio-inline-group" value="Yes" aria-expanded="false">' +
+                                      "Yes" +
+                                  '</label>' +
+                                  '<label class="block-label selectable" for="radio-inline-2">' +
+                                    '<input id="radio-inline-2" type="radio" name="radio-inline-group" value="No" aria-expanded="false">' +
+                                      'No' +
+                                  '</label>' +
+                                '</fieldset>' +
+                              '</form>'+
+                          '</p>' +
+                        '</div>';
+      var $containerAlternate = $('.report-a-problem-container');
+      $containerAlternate.before(toggleBlockAlternate);
+
+      $containerAlternate.find('#js-ab-test-title').text("Thanks. Your feedback has been recorded.");
+      $containerAlternate.find("#js-ab-test-paragraph").hide();
+      $containerAlternate.find('#js-ab-test-first-label').text("How could we improve this page?");
+      $containerAlternate.find('.js-ab-test-first-form').prop("type","textarea");
+      $containerAlternate.find('#js-ab-test-second-label').hide();
+      $containerAlternate.find('.js-ab-test-second-form').hide();
+
+
+      // Add a click handler for the toggle
+      $('.report-a-problem-toggle-wrapper :radio').on('click', function() {
+        $containerAlternate.show();
+      });
+    }
   });
-
 }());
