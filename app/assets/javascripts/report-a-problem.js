e5546f2fe7075c1963aad868289a699bd5cd9851
Paul Hayes <fofr@users.noreply.github.com>
2015-04-28 11:17:55 +0100

    Track when feedback is submitted

--- app/assets/javascripts/report-a-problem.js
+++ app/assets/javascripts/report-a-problem.js
@@ -22,6 +22,8 @@
           variant_1: {variantId: 1, callback: renderVariant}
         }
       });
+
+      $form.on("reportAProblemForm.success", this.trackSuccessfulFeedback.bind(this));
     } else {
       renderOriginal();
     }
@@ -113,6 +115,13 @@
 
     if (ReportAProblem.isBeingTestedOnThisPage() && typeof GOVUK.analytics === "object") {
       GOVUK.analytics.trackEvent('ab-test-report-a-problem', 'was-page-useful', {label: wasUseful.toLowerCase()});
+    }
+  };
+
+  ReportAProblem.prototype.trackSuccessfulFeedback = function(evt) {
+    var variant = this.multivariateTest.getCohort();
+    if (typeof GOVUK.analytics === "object") {
+      GOVUK.analytics.trackEvent('ab-test-report-a-problem', 'feedback-submitted', {label: variant});
     }
   };
 
